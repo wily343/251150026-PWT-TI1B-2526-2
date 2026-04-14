fb80788180f771fc7fde7344c8c9d7fbe6f07989
wily343 <will.anakepa@gmail.com>
2026-04-14 10:16:49 +0700

    PP

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">willy</a>
         </div>
       </div>
 
