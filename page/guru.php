2a8e0f8f98a4ca59f6886df32378e095289c0d47
wily343 <will.anakepa@gmail.com>
2026-04-28 09:28:45 +0700

    aksi

--- page/guru.php
+++ page/guru.php
@@ -9,9 +9,10 @@
 </div>
 
 <?php
+require_once __DIR__ . "/../config/koneksi.php";
 if(isset($_GET['action'])) {
     if($_GET['action'] == "hapus" && isset($_GET['Kd'])) {
-        $Kd =$_GET['Kd'];
+        $Kd = mysqli_real_escape_string($koneksi, $_GET['Kd']);
         $query = mysqli_query($koneksi, "DELETE FROM guru WHERE Kd_guru ='$Kd'");
         if ($query) {
             echo '
@@ -31,41 +32,42 @@ if(isset($_GET['action'])) {
                     <thead>
                         <tr>
                             <th>No</th>
-                            <th>Kd Guru</th>
-                            <th>Id Guru</th>
-                            <th>Nama Guru</th>
-                            <th>Jenis Kelamin</th>
-                            <th>Pendidikan Terakhir</th>
-                            <th>Nomor Hp</th>
-                            <th>Alamat</th>
+                            <th>id guru</th>
+                            <th>nip</th>
+                            <th>nama_guru</th>
+                            <th>jenis_kelamin</th>
+                            <th>alamat</th>
+                            <th>no_hp</th>
+                            <th>email</th>
                             <th>Aksi</th>
                         </tr>
                     </thead>
+                    <tbody>
                     <?php
                     $no = 0;
                     $query = mysqli_query($koneksi, "SELECT * FROM guru");
                     while ($result = mysqli_fetch_array($query) ) {
                         $no++;
                     ?>
-                        <tbody>
-                            <tr>
-                                <td><?= $no;?></td>
-                                <td><?=$result['Kd_guru']; ?></td>
-                                <td><?=$result['Id_guru']; ?></td>
-                                <td><?=$result['Nm_guru']; ?></td>
-                                <td><?=$result['Jenkel']; ?></td>
-                                <td><?=$result['Pend_terakhir']; ?></td>
-                                <td><?=$result['Hp']; ?></td>
-                                <td><?=$result['Alamat']; ?></td>
-                                <td>
-                                    <a href="index.php?page=guru&action=hapus&Kd=<?= $result['Kd_guru']?>" title="">
-                                        <span class="badge badge-danger">Hapus</span></a>
-                                    <a href ="index.php?page=edit_guru&Kd=<?= $result['Kd_guru']?>" title="">
-                                        <span class="badge badge-warning">Edit</span></a>
-                                </td>
-                            </tr>
-                        </tbody>
-                        <?php } ?>
+                        <tr>
+                            <td><?= $no;?></td>
+                            <td><?=$result['id_guru']; ?></td>
+                            <td><?=$result['nip']; ?></td>
+                            <td><?=$result['nama_guru']; ?></td>
+                            <td><?=$result['jenis_kelamin']; ?></td>
+                            <td><?=$result['alamat']; ?></td>
+                            <td><?=$result['no_hp']; ?></td>
+                            <td><?=$result['email']; ?></td>
+                            <td>
+                            <td>
+                                <a href="index.php?page=guru&action=hapus&Kd=<?= $result['Kd_guru']?>" title="">
+                                    <span class="badge badge-danger">Hapus</span></a>
+                                <a href ="index.php?page=edit_guru&Kd=<?= $result['Kd_guru']?>" title="">
+                                    <span class="badge badge-warning">Edit</span></a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                    </tbody>
                     </table>
                 </div>
             </div>
